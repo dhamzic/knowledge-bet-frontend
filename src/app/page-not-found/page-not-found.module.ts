6f235f75ee4f4712aa07033ed52028e7f4dfd274
Domagoj <37220260+dhamzic@users.noreply.github.com>
2022-04-09 16:21:31 +0200

    commit before angular cli update

--- src/app/page-not-found/page-not-found.module.ts
+++ src/app/page-not-found/page-not-found.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageNotFoundComponent } from './page-not-found.component';
 import { PageNotFoundRoutingModule } from './page-not-found-routing.module';
+import { PrimengModule } from '../shared/primeng.module';
 
 @NgModule({
     imports: [
         CommonModule,
+        PrimengModule,
         PageNotFoundRoutingModule
     ],
     declarations: [PageNotFoundComponent]
---
+++ src/app/shared/primeng.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+// import { TableModule } from 'primeng/table';
+// import { ToastModule } from 'primeng/toast';
+// import { ProgressSpinnerModule } from 'primeng/progressspinner';
+// import { DialogModule } from 'primeng/dialog';
+// import { InputTextareaModule } from 'primeng/inputtextarea';
+// import { InputTextModule } from 'primeng/inputtext';
+
+@NgModule({
+    imports: [
+        // ButtonModule,
+        // TableModule,
+        // ToastModule,
+        // ProgressSpinnerModule,
+        // DialogModule,
+        // InputTextareaModule,
+        // InputTextModule,
+    ],
+    exports: [
+        // ButtonModule,
+        // TableModule,
+        // ToastModule,
+        // ProgressSpinnerModule,
+        // DialogModule,
+        // InputTextareaModule,
+        // InputTextModule
+    ]
+})
+
+export class PrimengModule { }
